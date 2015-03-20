324febc1ff4718fa26df8bd10b251a6f7fded9f0
Ryan Graham <ryan@codingintrigue.co.uk>
2015-03-20 11:06:59 +0000

    - Replaced extend with new function - Replaced jQuery with native functions - Added CSS3 animations to popup box - Added strict mode - Added callbacks to .img and .load static methods to return a Messi instance

--- messi.js
+++ messi.js
@@ -14,91 +14,119 @@
 
 /* v1.3 Legacy Syntax */
 (function ($) {
-    function extend(target, source) {
-        var a = Object.create(target);
-        Object.keys(source).map(function (prop) {
-            prop in a && (a[prop] = source[prop]);
-        });
-        return a;
+    "use strict";
+    var extend = function ( defaults, options ) {
+        var extended = {};
+        var prop;
+        for (prop in defaults) {
+            if (Object.prototype.hasOwnProperty.call(defaults, prop)) {
+                extended[prop] = defaults[prop];
+            }
+        }
+        for (prop in options) {
+            if (Object.prototype.hasOwnProperty.call(options, prop)) {
+                extended[prop] = options[prop];
+            }
+        }
+        return extended;
     };
 
     var Messi = function (data, options) {
         // Extend our prototype with the options
         this.options = extend(Messi.prototype.options, options || {});
         // Create the main DOM element for this instance
-        this.messi = $(this.template);
+        this.messi = document.createElement("div");
+        this.messi.className = "messi";
+        this.messi.innerHTML = this.template;
         // Set the HTML content of our instance
         this.setContent(data);
 
         // If the title is not set, remove the titlebox element
-        if (this.options.title == null) this.findElement(".messi-titlebox").remove();
+        if (this.options.title == null) {
+            var titleBox = this.findElement(".messi-titlebox");
+            if(titleBox) titleBox.parentElement.removeChild(titleBox);
+        }
         else {
             // Otherwise add the title into the titlebox
-            this.findElement('.messi-title').append(this.options.title);
+            this.findElement(".messi-title").innerHTML = this.options.title;
 
             // If there are no buttons, autoclose is not set & the user wants a closeButton,
             // then add this into the titlebox
             if (this.options.buttons.length === 0 && !this.options.autoclose && this.options.closeButton) {
-                var close = $('<span class="messi-closebtn"></span>')
-                    .on('click', function () {
-                        this.hide();
-                    }.bind(this));
-                this.findElement(".messi-titlebox").prepend(close);
+                // Create the close button
+                var close = document.createElement("span");
+                close.className = "messi-closebtn";
+                close.addEventListener("click", function() {
+                    this.hide();
+                }.bind(this));
+                // Find the titlebox & insert the close button before it
+                var titleBox = this.findElement(".messi-titlebox");
+                titleBox.parentElement.insertBefore(close,  titleBox);
             }
-            ;
 
             // If the user specified a titlebox class, add it
-            if (this.options.titleClass != null) this.findElement('.messi-titlebox').addClass(this.options.titleClass);
+            if (this.options.titleClass != null) {
+                this.findElement(".messi-titlebox").className += " " + this.options.titleClass;
+            }
         }
 
         // If the user specified a width, assign it to the outer box
-        if (this.options.width != null) this.findElement('.messi-box').css('width', this.options.width);
+        if (this.options.width != null) {
+            this.findElement('.messi-box').style.width = this.options.width;
+        }
 
         // If the user specified buttons...
         if (this.options.buttons.length > 0) {
-
             for (var i = 0; i < this.options.buttons.length; i++) {
+                var button = this.options.buttons[i];
                 // Generate a class for each button, if specified
-                var cls = (this.options.buttons[i]["class"]) ? _this.options.buttons[i]["class"] : '';
+                var cls = button["class"] ? button["class"] : "";
                 // Generate the button DOM element
-                var btn = $('<div class="btnbox"><button class="btn ' + cls + '" href="#">' + this.options.buttons[i].label + '</button></div>');
+                var btnWrapper = document.createElement("div");
+                btnWrapper.className = "btnbox";
+                var btn = document.createElement("button");
+                btn.className = "btn " + cls;
+                btn.innerHTML = button.label;
+                btnWrapper.appendChild(btn);
                 // Bind onto the click for this button & pass through the value
-                btn.on('click', 'button', (function (self, value) {
+                btn.addEventListener('click', (function (self, button) {
                     return function () {
-                        var after = (self.options.callback != null) ? function () {
-                            self.options.callback(value);
-                        } : null;
+                        var after;
+                        if(button.clicked) after = button.clicked;
+                        else if(self.options.callback) after = function() {
+                            self.options.callback(button.val);
+                        };
                         self.hide(after);
                     };
-                }(this, this.options.buttons[i].val)));
+                }(this, button)));
 
                 // Append the button to the action holder
-                this.findElement('.messi-actions').append(btn);
-
+                this.findElement('.messi-actions').appendChild(btnWrapper);
             }
-            ;
-
         } else {
             // If no buttons are specified, remove the footbox
-            this.findElement('.messi-footbox').remove();
-
+            var footBox = this.findElement(".messi-footbox");
+            footBox.parentElement.removeChild(footBox);
         }
-        ;
 
         // If the modal option is set, and no modal wrapper is already present,
         // then create one
-        this.modal = (this.options.modal && !$(".messi-modal").length) ? $('<div class="messi-modal"></div>').css({
-            opacity: this.options.modalOpacity,
-            width: $(document).width(),
-            height: $(document).height(),
-            'z-index': this.options.zIndex + $('.messi').length
-        }).appendTo(document.body) : null;
+        this.modal = null;
+        if(this.options.modal) {
+            this.modal = document.createElement("div");
+            this.modal.className = "messi-modal";
+            this.modal.style.opacity = this.options.modalOpacity;
+            this.modal.style.width = document.documentElement.clientWidth;
+            this.modal.style.height = document.documentElement.clientHeight;
+            this.modal.style.zIndex = this.options.zIndex + document.querySelectorAll(".messi").length;
+            document.body.appendChild(this.modal);
+        }
 
         // If autoshow is set, show the dialog automatically
         if (this.options.show) this.show();
 
         // On window resize, trigger our resize event
-        $(window).on('resize', this.resize.bind(this));
+        window.addEventListener('resize', this.resize.bind(this));
 
         // Set up autoclose if required
         if (this.options.autoclose != null) {
@@ -106,7 +134,6 @@
                 this.hide();
             }.bind(this), this.options.autoclose);
         }
-        ;
 
         return this;
     };
@@ -130,28 +157,28 @@
             unload: true,                            // unload message after hide
             viewport: {top: '0px', left: '0px'},     // if not center message, sets X and Y position
             width: '500px',                          // message width
-            zIndex: 99999                            // message z-index
+            zIndex: 9000                            // message z-index
         },
-        template: '<div class="messi"><div class="messi-box"><div class="messi-wrapper"><div class="messi-titlebox"><span class="messi-title"></span></div><div class="messi-content"></div><div class="messi-footbox"><div class="messi-actions"></div></div></div></div></div>',
+        template: '<div class="messi-box"><div class="messi-wrapper"><div class="messi-titlebox"><span class="messi-title"></span></div><div class="messi-content"></div><div class="messi-footbox"><div class="messi-actions"></div></div></div></div>',
         content: '<div></div>',
         visible: false,
 
         findElement: function (selector) {
-            return $(selector, this.messi);
+            return this.messi.querySelector(selector);
         },
 
         setContent: function (data) {
-            this.findElement(".messi-content").css({
-                padding: this.options.padding,
-                height: this.options.height
-            }).empty().append(data);
+            var content = this.findElement(".messi-content");
+            content.style.padding = this.options.padding;
+            content.style.height = this.options.height;
+            if(typeof(data) == "string" || data instanceof String) content.innerHTML = data;
+            else content.appendChild(data);
         },
 
         viewport: function () {
-
             return {
-                top: (($(window).height() - this.messi.height()) / 2) + $(window).scrollTop() + "px",
-                left: (($(window).width() - this.messi.width()) / 2) + $(window).scrollLeft() + "px"
+                top: ((window.innerHeight - this.messi.clientHeight) / 2) + document.body.scrollTop + "px",
+                left: ((window.innerWidth - this.messi.clientWidth) / 2) + document.body.scrollLeft + "px"
             };
 
         },
@@ -160,20 +187,15 @@
 
             if (this.visible) return;
 
-            if (this.options.modal && this.modal != null) this.modal.show();
-            this.messi.appendTo(document.body);
+            if (this.options.modal && this.modal != null) this.modal.classList.add("shown");
+            document.body.appendChild(this.messi);
 
             // obtenemos el centro de la pantalla si la opción de centrar está activada
             if (this.options.center) this.options.viewport = this.viewport(this.findElement(".messi-box"));
-
-            this.messi.css({
-                top: this.options.viewport.top,
-                left: this.options.viewport.left,
-                'z-index': this.options.zIndex + $('.messi').length
-            }).show().animate({opacity: 1}, 300);
-
-            // cancelamos el scroll
-            //document.documentElement.style.overflow = "hidden";
+            this.messi.style.top = this.options.viewport.top;
+            this.messi.style.left = this.options.viewport.left;
+            this.messi.style.zIndex = this.options.zIndex + document.querySelectorAll(".messi").length;
+            this.messi.classList.add("shown");
 
             this.visible = true;
 
@@ -183,15 +205,13 @@
 
             if (!this.visible) return;
 
-            this.messi.animate({opacity: 0}, 300, function () {
-                if (this.options.modal && this.modal != null) this.modal.remove();
-                this.messi.css({display: 'none'}).remove();
-                // reactivamos el scroll
-
+            this.messi.classList.remove("shown");
+            setTimeout(function() {
+                if (this.options.modal && this.modal != null) this.modal.classList.remove("shown");
                 this.visible = false;
                 if (after) after.call();
                 if (this.options.unload) this.unload();
-            }.bind(this));
+            }.bind(this), 300);
 
             return this;
 
@@ -199,14 +219,15 @@
 
         resize: function () {
             if (this.options.modal) {
-                $('.messi-modal').css({width: $(document).width(), height: $(document).height()});
+                this.modal.width = document.documentElement.clientWidth;
+                this.modal.height = document.documentElement.clientHeight;
             }
-            ;
+
             if (this.options.center) {
                 this.options.viewport = this.viewport(this.findElement(".messi-box"));
-                this.messi.css({top: this.options.viewport.top, left: this.options.viewport.left});
+                this.messi.style.top = this.options.viewport.top;
+                this.messi.style.left = this.options.viewport.left;
             }
-            ;
         },
 
         toggle: function () {
@@ -216,103 +237,106 @@
 
         unload: function () {
             if (this.visible) this.hide();
-            $(window).off('resize', this.resize);
-            this.messi.remove();
+            window.removeEventListener('resize', this.resize);
+            this.messi.parentElement.removeChild(this.messi);
         }
 
     };
 
-    // llamadas especiales
-    extend(Messi, {
+    // Static methods
+    Messi.alert = function (data, callback, options) {
 
-        alert: function (data, callback, options) {
+        var buttons = [{label: 'OK', val: 'OK'}];
 
-            var buttons = [{id: 'ok', label: 'OK', val: 'OK'}];
+        options = extend({
+                closeButton: false, buttons: buttons, show: true, unload: true, callback: callback
+            }, options || {}
+        );
 
-            options = extend({
-                    closeButton: false, buttons: buttons, show: true, unload: true, callback: callback
-                }, options || {}
-            );
+        return new Messi(data, options);
+    };
 
-            return new Messi(data, options);
+    Messi.ask = function (data, callback, options) {
 
-        },
+        var buttons = [
+            {label: 'Yes', val: 'Y', "class": 'btn-success'},
+            {label: 'No', val: 'N', "class": 'btn-danger'},
+        ];
 
-        ask: function (data, callback, options) {
+        options = extend({
+                closeButton: false, modal: true, buttons: buttons, show: true, unload: true, callback: callback
+            }, options || {}
+        );
 
-            var buttons = [
-                {id: 'yes', label: 'Yes', val: 'Y', "class": 'btn-success'},
-                {id: 'no', label: 'No', val: 'N', "class": 'btn-danger'},
-            ];
+        return new Messi(data, options);
 
-            options = extend({
-                    closeButton: false, modal: true, buttons: buttons, show: true, unload: true, callback: callback
-                }, options || {}
-            );
+    };
 
-            return new Messi(data, options);
+    Messi.img = function (src, options, onComplete) {
 
-        },
+        var img = new Image();
+        img.addEventListener("load", function () {
 
-        img: function (src, options) {
+            var vp = {width: window.innerWidth - 50, height: window.innerHeight - 50};
+            var ratio = (this.width > vp.width || this.height > vp.height) ? Math.min(vp.width / this.width, vp.height / this.height) : 1;
 
-            var img = new Image();
+            img.style.width = this.width * ratio;
+            img.style.height = this.height * ratio;
 
-            $(img).load(function () {
+            var opts = extend(options || {}, {
+                title: "Image",
+                show: true,
+                unload: true,
+                closeButton: true,
+                width: this.width * ratio,
+                height: this.height * ratio,
+                padding: 0
+            });
+            var messi = new Messi(img, opts);
+            if(typeof onComplete == "function") onComplete.call(messi);
+        });
+        img.addEventListener("error", function () {
+            throw Error('Error loading image ' + src);
+        });
+        img.setAttribute("src", src);
+    };
 
-                var vp = {width: $(window).width() - 50, height: $(window).height() - 50};
-                var ratio = (this.width > vp.width || this.height > vp.height) ? Math.min(vp.width / this.width, vp.height / this.height) : 1;
-
-                $(img).css({width: this.width * ratio, height: this.height * ratio});
-
-                options = extend(options || {}, {
-                    show: true,
-                    unload: true,
-                    closeButton: true,
-                    width: this.width * ratio,
-                    height: this.height * ratio,
-                    padding: 0
-                });
-                new Messi(img, options);
-
-            }).error(function () {
-
-                console.log('Error loading ' + src);
-
-            }).attr('src', src);
-
-        },
-
-        load: function (url, options) {
-
-            options = jQuery.extend(options || {}, {show: true, unload: true, params: {}});
-
-            var request = {
-                url: url,
-                data: options.params,
-                dataType: 'html',
-                cache: false,
-                error: function (request, status, error) {
-                    console.log(request.responseText);
-                },
-                success: function (html) {
-                    new Messi(html, options);
-                }
-            };
-
-            $.ajax(request);
-
+    Messi.load = function (url, options, onComplete) {
+        var ajaxOptions = extend(options || {}, {show: true, unload: true});
+        var requestParams = ajaxOptions["params"];
+        var requestUri = url;
+        if(typeof requestParams === "object") {
+            var obj = extend(requestParams, {"___ts": new Date().getTime()});
+            requestParams = Object.keys(obj).reduce(function(arr, key) {
+                arr.push(key + "=" + encodeURIComponent(obj[key]));
+                return arr;
+            }, []).join("&");
         }
-
-    });
+        requestUri += (~requestUri.indexOf("?") ? "&" : "?") + requestParams;
+        var request = new XMLHttpRequest();
+        request.addEventListener("load", function(e) {
+            if(e.target.status === 200) {
+                console.log(e.target.responseText);
+                var messi = new Messi(e.target.responseText, ajaxOptions);
+                if(typeof onComplete == "function") onComplete.call(messi);
+            }
+        });
+        request.addEventListener("error", function(request) {
+            throw Error(request.responseText);
+        });
+        request.open("GET", requestUri);
+        request.send();
+    };
 
     // Export messi
     window.Messi = Messi;
 
-    // jQuery wrapper
-    $.fn.messi = function (options) {
-        this.each(function () {
-            new Messi(this.outerHTML, options);
-        }).css("display", "none");
-    };
-}(jQuery));
+    if($) {
+        // jQuery wrapper
+        $.fn.messi = function (options) {
+            this.each(function () {
+                new Messi(this.outerHTML, options);
+            }).css("display", "none");
+        };
+    }
+}(window.jQuery));
